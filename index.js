a7e4508d681c2ee59c916352254380eda46bf2b8
Alpx <alpxit@gmail.com>
2026-09-08 17:37:02 +0300

    switchers width a few reduced

--- index.js
+++ index.js
@@ -1001,7 +1001,6 @@ function prepareControls() {
             if (vers !== cacheNames[0]) {
               labelVersion.addClass('text-warning');
               labelVersion.css('font-size', '0.8em');
-              labelVersion.css('top', '-0.6em');
               btnUpdateVersion.removeClass('d-none');
               btnUpdateVersion.click(function () {
                 caches.delete(cacheNames);
